276d2731748afb27712c8599599065dcbea85748
Chidu <chidganahegade@gmail.com>
2020-09-26 22:48:23 +0530

    Header component complete

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,43 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 function Header() {
-    return (
-        <div className="header">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1920px-Amazon_logo.svg.png" alt=""></img>
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1920px-Amazon_logo.svg.png"
+        alt=""
+      ></img>
+      <div className="header__search">
+        <input
+          placeholder="Search Amazon"
+          type="text"
+          className="header__searchInput"
+        ></input>
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello chidu</span>
+          <span className="header__optionLineTwo">SignIn</span>
         </div>
-    )
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
