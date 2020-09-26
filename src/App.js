f967e02a02054b0fdb2eca7ac972d463c17e9733
Chidu <chidganahegade@gmail.com>
2020-09-26 21:29:17 +0530

    clean up complete

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-     
+     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1920px-Amazon_logo.svg.png" alt=""></img>
     </div>
   );
 }
