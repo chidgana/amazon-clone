e7e3725f11cc64a20c018043d32634d8df001ab3
Chidu <chidganahegade@gmail.com>
2020-09-26 21:36:50 +0530

    my commits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import Header from './Header'
+import Home from './Home'
 import './App.css';
 
 function App() {
   return (
     <div className="app">
-     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1920px-Amazon_logo.svg.png" alt=""></img>
+    <Header/>
+    <Home/>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+function Header() {
+    return (
+        <div className="header">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1920px-Amazon_logo.svg.png" alt=""></img>
+        </div>
+    )
+}
+
+export default Header
