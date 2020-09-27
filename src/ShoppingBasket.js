6bdcee6a9e4af67a6d2f17ee0825b65839c5ef67
Chidu <chidganahegade@gmail.com>
2020-09-28 00:01:03 +0530

    my commits

--- src/ShoppingBasket.js
+++ src/ShoppingBasket.js
@@ -4,7 +4,31 @@ import { useStateValue } from "./StateProvider";
 
 function ShoppingBasket() {
   const [{ basket }, dispacth] = useStateValue();
-  return <div className="shoppingBasket"></div>;
+  const removeFromBasket = () => {};
+  return (
+    <>
+      {basket.map((item) => (
+        <div className="shoppingBasket">
+          <div className="shoppingBasket__info">
+            <p>{item.title}</p>
+            <p className="shoppingBasket__price">
+              <small>$</small>
+              <strong>{item.price}</strong>
+            </p>
+            <div className="shoppingBasket__rating">
+              {Array(item.rating)
+                .fill()
+                .map((_, i) => (
+                  <p>⭐</p>
+                ))}
+            </div>
+          </div>
+          <img src={item.image} alt=""></img>
+          <button onClick={removeFromBasket}>Remove from basket</button>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default ShoppingBasket;
