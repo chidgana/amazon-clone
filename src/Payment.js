7b24ed04e6b918224a793f9c7a7d3fff8bd252e2
Chidu <chidganahegade@gmail.com>
2020-09-30 18:24:44 +0530

    git commit

--- src/Payment.js
+++ src/Payment.js
@@ -1,9 +1,47 @@
 import React from "react";
 import "./Payment.css";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
+import { Link } from "react-router-dom";
 function Payment() {
+  const [{ basket, user }] = useStateValue();
   return (
     <div className="payment">
-      <h1> i am payment</h1>
+      <div className="payment__container">
+        <h1>Checkout {<Link to="/checkout">{basket?.length} items</Link>} </h1>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivary Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 React lane</p>
+            <p>sagar,karntaka</p>
+          </div>
+        </div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review Items and Delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                image={item.image}
+                title={item.title}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details"></div>
+        </div>
+      </div>
     </div>
   );
 }
