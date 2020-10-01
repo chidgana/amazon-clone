a14306bcf38b99adf2c445ef9eb596c473dc0a9c
Chidu <chidganahegade@gmail.com>
2020-10-01 19:30:07 +0530

    incomplete due to payment issues

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,12 @@
-// const functions = require("firebase-functions");
+const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const { request, response } = require("express");
 const stripe = require("stripe")(
   "sk_test_51HX4uhLnaJQDbJfvaf2cWj46jZJFptN3hw6hKZs8T85fe61EYyQ3WCllt9ZXZ82m0HTDgm6TKqrEQYKFNdEXnqgO00x7EFxXSX"
 );
-
+//firebase deploy --only functions
+//it is incomplete for now
 //API
 
 //Appconfig
